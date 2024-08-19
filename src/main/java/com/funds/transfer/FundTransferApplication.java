c7b7c731b538fcd411f43886fced515faf99df70
Varun Kaushik <varunkaushik@MacBook-Pro.fritz.box>
2024-08-19 09:42:21 +0200

    added base structure with dependencies

---
+++ src/main/java/com/funds/transfer/FundTransferApplication.java
@@ -0,0 +1,13 @@
+package com.funds.transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/funds/transfer/FundTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funds.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
